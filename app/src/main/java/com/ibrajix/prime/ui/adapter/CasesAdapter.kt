99c5ddc7199a60af80760e6e36482b7464200034
ibrajix <ibrajix@gmail.com>
2022-02-06 18:36:44 +0100

    Finished testing for mobile variant and fixed background color of items and text view in card when clicked

--- app/src/main/java/com/ibrajix/prime/ui/adapter/CasesAdapter.kt
+++ app/src/main/java/com/ibrajix/prime/ui/adapter/CasesAdapter.kt
@@ -3,6 +3,7 @@ package com.ibrajix.prime.ui.adapter
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
@@ -16,6 +17,11 @@ import com.ibrajix.prime.room.Case
 class CasesAdapter(private val onClickListener: OnCaseClickListener) : ListAdapter<Case, CasesAdapter.CasesViewHolder>(CasesDiffCallback()) {
 
     private var cardViewList = mutableListOf<CardView>()
+    private var textDeliveryDateList = mutableListOf<TextView>()
+    private var textFirstClientNameList = mutableListOf<TextView>()
+    private var textSecondClientNameList = mutableListOf<TextView>()
+    private var textGeneratedDateList = mutableListOf<TextView>()
+    private var textVsList = mutableListOf<TextView>()
 
     class CasesViewHolder private constructor(private val binding: RcvMainContentCasesBinding) : RecyclerView.ViewHolder(binding.root){
 
@@ -23,7 +29,7 @@ class CasesAdapter(private val onClickListener: OnCaseClickListener) : ListAdapt
 
             binding.txtDeliveryDate.text = case.date
             binding.txtFirstClientName.text = case.firstClientName
-            binding.txtSecondClientName.text = case.firstClientName
+            binding.txtSecondClientName.text = case.secondClientName
             binding.txtGeneratedDate.text = case.genId
 
 
@@ -64,17 +70,60 @@ class CasesAdapter(private val onClickListener: OnCaseClickListener) : ListAdapt
             holder.bindCase(item)
         }
 
-        if (!cardViewList.contains(holder.itemView.findViewById(R.id.container))) {
-            cardViewList.add(holder.itemView.findViewById(R.id.container))
+        if (
+            !cardViewList.contains(holder.itemView.findViewById(R.id.container)) ||
+            !textDeliveryDateList.contains(holder.itemView.findViewById(R.id.txt_delivery_date)) ||
+            !textFirstClientNameList.contains(holder.itemView.findViewById(R.id.txt_first_client_name)) ||
+            !textSecondClientNameList.contains(holder.itemView.findViewById(R.id.txt_second_client_name)) ||
+            !textGeneratedDateList.contains(holder.itemView.findViewById(R.id.txt_generated_date)) ||
+            !textVsList.contains(holder.itemView.findViewById(R.id.txt_vs))
+        )
+
+            {
+                cardViewList.add(holder.itemView.findViewById(R.id.container))
+                textDeliveryDateList.add(holder.itemView.findViewById(R.id.txt_delivery_date))
+                textFirstClientNameList.add(holder.itemView.findViewById(R.id.txt_first_client_name))
+                textSecondClientNameList.add(holder.itemView.findViewById(R.id.txt_second_client_name))
+                textGeneratedDateList.add(holder.itemView.findViewById(R.id.txt_generated_date))
+                textVsList.add(holder.itemView.findViewById(R.id.txt_vs))
         }
+
 
          holder.itemView.setOnClickListener {
              if (item != null){
+
                  for (cardView in cardViewList){
                      cardView.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.card_unselected))
                  }
+
+                 for (txtDeliveryDate in textDeliveryDateList){
+                     txtDeliveryDate.setTextColor(ContextCompat.getColor(holder.itemView.context, R.color.black))
+                 }
+
+                 for (txtFirstClientName in textFirstClientNameList){
+                     txtFirstClientName.setTextColor(ContextCompat.getColor(holder.itemView.context, R.color.black))
+                 }
+
+                 for (txtSecondClientName in textSecondClientNameList){
+                     txtSecondClientName.setTextColor(ContextCompat.getColor(holder.itemView.context, R.color.black))
+                 }
+
+                 for (txtGeneratedDate in textGeneratedDateList){
+                     txtGeneratedDate.setTextColor(ContextCompat.getColor(holder.itemView.context, R.color.black))
+                 }
+
+                 for (txtVs in textVsList){
+                     txtVs.setTextColor(ContextCompat.getColor(holder.itemView.context, R.color.black))
+                 }
+
                  holder.itemView.findViewById<CardView>(R.id.container).setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.main_orange))
+                 holder.itemView.findViewById<TextView>(R.id.txt_delivery_date).setTextColor(ContextCompat.getColor(holder.itemView.context, R.color.white))
+                 holder.itemView.findViewById<TextView>(R.id.txt_first_client_name).setTextColor(ContextCompat.getColor(holder.itemView.context, R.color.white))
+                 holder.itemView.findViewById<TextView>(R.id.txt_second_client_name).setTextColor(ContextCompat.getColor(holder.itemView.context, R.color.white))
+                 holder.itemView.findViewById<TextView>(R.id.txt_generated_date).setTextColor(ContextCompat.getColor(holder.itemView.context, R.color.white))
+                 holder.itemView.findViewById<TextView>(R.id.txt_vs).setTextColor(ContextCompat.getColor(holder.itemView.context, R.color.white))
                  onClickListener.onClickCase(item)
+
              }
          }
     }
